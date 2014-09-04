f1fc8e64ceed23993f67375f4a6d86d2128c36e3
SeedPaths-User <lessdismissive@email.com>
2014-09-04 13:04:10 -0600

    Updates

---
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessThatNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessThatNumber();
+
+            Console.ReadKey();
+        }
+        static void GuessThatNumber()
+        
+        {
+
+
+
+            Random rng = new Random();
+        
+        
+        int userNum;
+            Console.WriteLine("Enter a number.");
+                userNum = Convert.ToInt32(Console.ReadLine());
+
+            int randomNum = rng.Next(1, 101);
+            Console.WriteLine(randomNum);
+
+                if (userNum == randomNum)
+                {
+                    Console.WriteLine("Your number " + userNum + " matches");
+                }
+                else if (userNum > randomNum)
+                {
+                    Console.WriteLine("Your number " + userNum + " is to great");
+                }
+                else if (userNum < randomNum)
+                {
+                    Console.WriteLine("Your number " + userNum + "is lower than " + randomNum);
+                }
+       
+
+
+
+
+        
+        }
+
+
+    }
+}
